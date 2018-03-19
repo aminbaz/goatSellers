629d2e953ca4e5633f00db6063ebe8dcc55cfe01
Gunners69400 <escalier.justin@gmail.com>
2018-03-19 17:38:55 +0100

    Implemented FXML system

--- src/presentation/ClientUI.java
+++ src/presentation/ClientUI.java
@@ -2,65 +2,52 @@ package presentation;
 
 import business_logic.models.User;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class ClientUI extends Application implements WindowManager{
+public class ClientUI extends Application{
 
-	private User connectedUser;
-	private Frame myFrame;
-	private Stage myStage;
+	private static User myUser=null;
+	private static Stage myStage=null;
 	
 	public ClientUI() {
-		myFrame = new LoginFrame(this);
+		
 	}
 	
-	public User getConnectedUser() {
-		return connectedUser;
-	}
-
-	public void setConnectedUser(User connectedUser) {
-		this.connectedUser = connectedUser;
-	}
-
-	public Frame getMyFrame() {
-		return myFrame;
-	}
-
-	public void setMyFrame(Frame myFrame) {
-		this.myFrame = myFrame;
-	}
-
-	@Override
-	public void setUser(User user) {
-		// TODO Auto-generated method stub
-		this.connectedUser=user;
-	}
-	
-	@Override
-	public void handleMessageFromFrame(String message) {
-		// TODO Auto-generated method stub
-		if(message.equals("Login Club")) {
-			setMyFrame(new ClubFrame(this));
-			myStage.setScene(myFrame.initScene());
-			myStage.show();
-		}
-	}
-
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 		myStage=primaryStage;
 		primaryStage.setTitle("Goat Sellers");
-		primaryStage.setScene(myFrame.initScene());
-		primaryStage.show();
-	}
-
-	@Override
-	public void updateStage(Scene scene) {
-		myStage.setScene(scene);
+		
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(ClientUI.class.getResource("login.fxml"));
+        Parent root=loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
 	}
 	
+	
+	
+	public static User getMyUser() {
+		return myUser;
+	}
+
+	public static void setMyUser(User myUser) {
+		ClientUI.myUser = myUser;
+	}
+
+	public static Stage getMyStage() {
+		return myStage;
+	}
+
+	public static void setMyStage(Stage myStage) {
+		ClientUI.myStage = myStage;
+	}
+
 	public static void main (String[] args){
 		launch(args);
 	}
---
+++ src/presentation/loginController.java
@@ -0,0 +1,59 @@
+package presentation;
+
+import java.io.IOException;
+
+import business_logic.facades.LoginFacade;
+import business_logic.models.Authority;
+import business_logic.models.Club;
+import business_logic.models.SuperAdmin;
+import business_logic.models.User;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class loginController {
+
+	private LoginFacade myFacade;
+	
+	public loginController() {
+		myFacade=new LoginFacade();
+	}
+	
+	@FXML private TextField fieldMail;
+	
+	@FXML private PasswordField fieldPassword;
+	
+	@FXML protected void handleLogin(ActionEvent event) {
+		User myUser=myFacade.login(fieldMail.getText(), fieldPassword.getText());
+		if(myUser !=null) {
+	        FXMLLoader loader = new FXMLLoader();
+			ClientUI.setMyUser(myUser);
+			if(myUser instanceof Club) {
+				System.out.println("true club");
+		        loader.setLocation(ClientUI.class.getResource("accueilClub.fxml"));
+			}else if(myUser instanceof Authority) {
+				System.out.println("true authority");
+		        loader.setLocation(ClientUI.class.getResource("authority_accueil.fxml"));
+			}else if(myUser instanceof SuperAdmin) {
+				System.out.println("true superadmin");
+			}else {
+				System.out.println("true user");
+			}
+	        Parent root=null;
+			try {
+				root = loader.load();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        Scene scene = new Scene(root);
+	        ClientUI.getMyStage().setScene(scene);
+		}else {
+			System.out.println("false");
+		}
+	}
+}
