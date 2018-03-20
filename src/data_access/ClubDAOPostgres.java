dfe1a3b602bf8d8af1a6473b03313ce159493f2f
Gunners69400 <escalier.justin@gmail.com>
2018-03-20 12:26:52 +0100

    Add methods ClubDAO

--- src/data_access/ClubDAOPostgres.java
+++ src/data_access/ClubDAOPostgres.java
@@ -1,19 +1,58 @@
 package data_access;
 
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import business_logic.models.Sale;
+import business_logic.models.User;
 
 public class ClubDAOPostgres extends ClubDAO{
 
+	PostgresJDBC db;
+	
+	public ClubDAOPostgres(){
+		db = PostgresJDBC.getInstance();
+	}
+	
 	@Override
 	public Sale[] getAllPurchases(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Sale purchases[] = null;
+		int cpt=0;
+		
+		String query="SELECT * FROM public.\"Sale\" WHERE buyer="+id;
+		ResultSet result=db.makeQuery(query);
+		try {
+			while(result.next()) {
+				Sale mySale = new Sale(result.getInt("id_sale"),result.getInt("amount_sale"),result.getDate("sale_date"),result.getInt("seller"),result.getInt("buyer"),result.getInt("player"));
+				purchases[cpt] = mySale;
+				cpt=cpt+1;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return purchases;
 	}
 
 	@Override
 	public Sale[] getAllSales(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Sale sales[] = null;
+		int cpt=0;
+		
+		String query="SELECT * FROM public.\"Sale\" WHERE seller="+id;
+		ResultSet result=db.makeQuery(query);
+		try {
+			while(result.next()) {
+				Sale mySale = new Sale(result.getInt("id_sale"),result.getInt("amount_sale"),result.getDate("sale_date"),result.getInt("seller"),result.getInt("buyer"),result.getInt("player"));
+				sales[cpt] = mySale;
+				cpt=cpt+1;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return sales;
 	}
 	
 }
