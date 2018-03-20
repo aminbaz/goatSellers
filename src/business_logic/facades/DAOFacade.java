f466d9fc323071e5d8fd2e481413ca3cc363ed13
Gunners69400 <escalier.justin@gmail.com>
2018-03-20 10:01:33 +0100

    fix DAOFacade

--- src/business_logic/facades/DAOFacade.java
+++ src/business_logic/facades/DAOFacade.java
@@ -2,6 +2,7 @@ package business_logic.facades;
 
 import business_logic.factories.DAOFactory;
 import business_logic.factories.PostgresDAOFactory;
+import data_access.PlayerDAO;
 import data_access.UserDAO;
 
 public class DAOFacade {
@@ -13,5 +14,9 @@ public class DAOFacade {
 	public UserDAO getUserDAO() {
 		return this.getDAOFactory().getUserDAO();
 	}
+	
+	public PlayerDAO getPlayerDAO() {
+		return this.getDAOFactory().getPlayerDAO();
+	}	
 	
 }
