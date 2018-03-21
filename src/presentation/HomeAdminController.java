12c5f29709a67715b98fd5f876549aab03a1c405
Amine <Amine@162.38.38.152>
2018-03-21 16:28:46 +0100

    handle home page admin

--- src/presentation/HomeAdminController.java
+++ src/presentation/HomeAdminController.java
@@ -1,5 +1,37 @@
 package presentation;
 
-public class HomeAdminController {
+import java.io.IOException;
 
+import business_logic.facades.HomeAdminFacade;
+import business_logic.facades.LoginFacade;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HomeAdminController {
+	
+	private HomeAdminFacade myFacade;
+	
+	public HomeAdminController() {
+		myFacade = new HomeAdminFacade();
+	}
+	
+	@FXML protected void handleLogOut(ActionEvent event) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(ClientUI.class.getResource("login.fxml"));
+        Parent root=null;
+		try {
+			root = loader.load();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        Scene scene = new Scene(root);
+        ClientUI.getMyStage().setScene(scene);
+	}
+	
 }
